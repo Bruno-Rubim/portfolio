0a14a81d9aaa64ff9214870d96ad0a9e17e514a6
Bruno Rubim <bruno.rubim924@gmail.com>
2025-12-12 22:56:25 -0300

    Added pt translations

--- scripts/translation.js
+++ scripts/translation.js
@@ -1,4 +1,8 @@
 const texts = {
+  back: {
+    en: "Back",
+    pt: "Voltar",
+  },
   "portfolio-title": {
     en: "Bruno Rubim's portfolio",
     pt: "Portifólio de Bruno Rubim",
@@ -8,12 +12,16 @@ const texts = {
     pt: "Olá, bem vind@ ao meu portifólio! Aqui você encontra alguns de meus trabalhos em projetos diferentes.",
   },
   "welcome-2": {
-    en: "They will be separated in different sections, depending which area of my work is of most interest.",
-    pt: "",
+    en: "They will be separated in different sections, depending which area I worked on.",
+    pt: "Eles estão separados em diferentes seções, de acordo com a área de trabalho na qual eu atuei.",
   },
   "welcome-3": {
     en: "Some projects will be featured in multiple sections given that my work on them involved different areas of art and design.",
-    pt: "",
+    pt: "Alguns projetos estarão em multiplas seções, dado que meu trabalho neles envolveu áreas diferentes de arte e design.",
+  },
+  "welcome-4": {
+    en: "Currently, the only populated section is Game Design",
+    pt: "Atualmente, a única seção com conteúdo é Game Design.",
   },
   "game-design-intro": {
     en: "My works of Game Design",
@@ -28,20 +36,20 @@ const texts = {
     pt: "Nota: Por contas de problemas com hospedagem, a plataforma pode demorar para carregar.",
   },
   "perito-1": {
-    en: "This was part of my final project as a system development student at <a href=''>IFPR</a>, an educational web game made with javascript to teach about malicious emails.",
-    pt: "",
+    en: "This was part of my final project as a system development student at <a href=''>IFPR</a>, an educational web game made with javascript to teach about malicious e-mails.",
+    pt: "Esse foi parte do meu trabalho final como aluno do curso Tecnólogo em Análise e Desenvolvimento de Sistemas no <a href=''>IFPR</a>, um jogo web feito em javascript para ensinar sobre e-mails maliciosos.",
   },
   "perito-2": {
-    en: "The player is faced with a sequence of levels, each with a list of emails they must classify according to the taught rules. Each level introduces new rules and emails become increasingly subtle, having a sense of progression and allowing them to reach the subtleness of their real life counterparts.",
-    pt: "",
+    en: "The player is faced with a sequence of levels, each with a list of e-mails they must classify according to the taught rules. Each level introduces new rules and the e-mails become increasingly subtle, having a sense of progression and allowing them to reach the subtleness of their real life counterparts.",
+    pt: "O jogador se depara com uma sequência de níveis, cada com uma lista de e-mails que eles devem classificar de acordo com as regras ensinadas. Cada nível introduz novas regras e os e-mails ficam mais sutis, dando uma progressão e permitindo que os e-mails chegem à sutileza dos encontrados na vida real.",
   },
   "perito-3": {
-    en: "E-mails were composed of elements, the sender's name, e-mail address and picture, as well as the text content of the e-mail. Pointing out suspicious elments according to the criterea taught through the game results in higher scores at the end of the level.",
-    pt: "",
+    en: "E-mails are composed of elements: the sender's name, e-mail address and picture, as well as the text content of the e-mail. Pointing out suspicious elments according to the criterea taught through the game results in higher scores at the end of the level.",
+    pt: "E-mails são compostos de elementos: o nome do remetente, endereço de e-mail e foto, além do conteúdo de texto do e-mail. Apontar elementos suspeitos de acordo com os critérios ensinados durante o jogo resulta em uma pontuação maior no final do nível.",
   },
   "perito-4": {
     en: "To engage the players, the platform where we featured the game was built with a leaderboard. The sum of the highscore of each completed level resuletd on the value shown on the leaderbord, incentivizing players to perfect their skills on analyzing and spotting suspicious elements.",
-    pt: "",
+    pt: "Para o engajamento dos jogadores, a plataforma que fizemos possúi um placar de pontuações. A soma da pontuação recorde em cada nível resulta no valor inserido no placar, incentivando os jogadores a aperfeiçoar suas abilidades de analize e identificar elementos suspeitos.",
   },
 };
 
