a439b11975b6e14eb3f1412da6f51ffafbcd49a6
Bruno Rubim <bruno.rubim924@gmail.com>
2025-12-18 15:01:49 -0300

    add wip art section for perito updated some project titles with links

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,19 +1,27 @@
-  function bindCarousel(elementRef, buttonLeftRef, buttonRightRef, srcList) {
-    let currentImgId = 0;
-    function carouselShift(imgElement, num) {
-      currentImgId += num;
-      if (currentImgId >= srcList.length) {
-        currentImgId = 0;
-      } else if (currentImgId < 0) {
-        currentImgId = srcList.length - 1;
-      }
-      imgElement.src = srcList[currentImgId];
+/**
+ * Binds the logic of carousel to a given image element and two buttons
+ * uses a given source list for the images
+ * @param {string} imageId
+ * @param {string} buttonLeftId
+ * @param {string} buttonRightId
+ * @param {string[]} srcList
+ */
+function bindCarousel(imageId, buttonLeftId, buttonRightId, srcList) {
+  let currentImgId = 0;
+  function carouselShift(imgElement, num) {
+    currentImgId += num;
+    if (currentImgId >= srcList.length) {
+      currentImgId = 0;
+    } else if (currentImgId < 0) {
+      currentImgId = srcList.length - 1;
     }
-    const element = document.querySelector(elementRef);
-    document.querySelector(buttonLeftRef).onclick = () => {
-      carouselShift(element, -1);
-    };
-    document.querySelector(buttonRightRef).onclick = () => {
-      carouselShift(element, 1);
-    };
+    imgElement.src = srcList[currentImgId];
   }
+  const element = document.querySelector(imageId);
+  document.querySelector(buttonLeftId).onclick = () => {
+    carouselShift(element, -1);
+  };
+  document.querySelector(buttonRightId).onclick = () => {
+    carouselShift(element, 1);
+  };
+}
--- scripts/translation.js
+++ scripts/translation.js
@@ -1,3 +1,5 @@
+const IFPRLINK = "<a href='https://ifpr.edu.br/' target='_blank'>IFPR</a>";
+
 const texts = {
   wip: {
     en: "[Content for this section is in progress]",
@@ -60,16 +62,16 @@ const texts = {
     pt: "Ver Design do jogo",
   },
   "perito-title": {
-    en: "Perito dos E-mails (E-mail expert)",
-    pt: "Perito dos E-mails",
+    en: `<a href="https://cibernauta-frontend.vercel.app/game/1" target="_blank">Perito dos E-mails (E-mail expert)</a>`,
+    pt: `<a href="https://cibernauta-frontend.vercel.app/game/1" target="_blank">Perito dos E-mails</a>`,
   },
   "perito-note": {
-    en: "Note: The project and game are currently only avaliable in brazilian portuguese.<br>Due to issues in hosting the platform may have long loading times.",
-    pt: "Nota: Por contas de problemas com hospedagem, a plataforma pode demorar para carregar.",
+    en: "Notes:<br>The platform requires a login to access the game.<br>The project and game are currently only avaliable in brazilian portuguese.<br>Due to issues in hosting the platform may have long loading times.",
+    pt: "Notas:<br>A plataforma requer um login para acessar o jogo.<br>Por contas de problemas com hospedagem, a plataforma pode demorar para carregar.",
   },
   "perito-gen-1": {
-    en: "This was part of my final project as a system development student at <a href=''>IFPR</a>, an educational web game that ran on Javascript to teach about malicious e-mails.",
-    pt: "Esse foi parte do meu trabalho final como aluno do curso Tecnólogo em Análise e Desenvolvimento de Sistemas no <a href=''>IFPR</a>, um jogo web feito em Javascript para ensinar sobre e-mails maliciosos.",
+    en: `This was part of my final project as a system development student at ${IFPRLINK}, an educational web game that ran on Javascript to teach about malicious e-mails.`,
+    pt: `Esse foi parte do meu trabalho final como aluno do curso Tecnólogo em Análise e Desenvolvimento de Sistemas no ${IFPRLINK}, um jogo web feito em Javascript para ensinar sobre e-mails maliciosos.`,
   },
   "perito-design-1": {
     en: "The player is faced with a sequence of levels, each with a list of e-mails they must classify according to the taught rules. Each level introduces new rules and the e-mails become increasingly subtle, having a sense of progression and allowing them to reach the subtleness of their real life counterparts.",
@@ -107,17 +109,53 @@ const texts = {
     en: `A file called "gameManager" creates a gameState, as well as a canvasObject with the given HTML canvas element reference. With both instances it starts a loop using requestAnimationFrame, where it calls the functions to update the gameState instance and to render its currentScene using the canvasObject.`,
     pt: `Um arquivo chamado "Gerenciador do Jogo" cria um Estado de Jogo e um Objeto de Canvas com uma dada referência de um elemento HTML de canvas. Com ambas instâncias um loop é iniciado usando requestAnimationFrame, que chama as funções para atualizar o Estado de Jogo e renderizar sua Cena Atual usando o Objeto de Canvas.`,
   },
+  "perito-art-1": {
+    en: `The game takes place inside a 2000's style computer, reminiscent of operating systems like <a href="https://en.wikipedia.org/wiki/Windows_XP" target="_blank">Windows XP</a>. Pixel art was chosen as the medium with a total screen size of 352x256. The game's actual screen size could scale up (without decimal values) to 3 times its size, keeping it pixel perfect but still being easily visible on the screen. For collor palette it was chosen<a href="https://www.tumblr.com/go-crag-go/151152049181/grafxkids-today-land-palette-release" target="_blank">"GrafxKid’s Today Land"</a>, with some small aditions, both for its pleasing color tones and simmilarity to the chosen visual style.`,
+    pt: ``,
+  },
+  "perito-art-2": {
+    en: `The game was devided into several screens, those being the desktop, saves, level selection, email, score and configuration screens.`,
+    pt: ``,
+  },
+  "perito-art-desktop": {
+    en: `Starting from the desktop screen, icons representing different actions and travel between screens are featured in a solid blue background.`,
+    pt: ``,
+  },
+  "perito-art-icon-mail": {
+    en: `Icon for the level selection screen<br>A mail envelope with the 3 buttons of e-mail classification used during levels.`,
+    pt: ``,
+  },
+  "perito-art-icon-saves": {
+    en: `Icon for the save selection screen<br>A box with user avatars icons, similar to that screen's visual design.`,
+    pt: ``,
+  },
+  "perito-art-icon-save": {
+    en: `Icon for manual saving<br>A floppy disc, commonly associated with saving data.`,
+    pt: ``,
+  },
+  "perito-art-icon-options": {
+    en: `Icon for the options screen<br>A set of gears, commonly used for settings and options.`,
+    pt: ``,
+  },
+  "perito-art-saves": {
+    en: `Derived from profile selection in old computers and how often diferent save slots are used as the different saves of different players, the save selection screen is a blue background with avatar icons, each avatar representing a save slot. An empty save being represented in gray and used saves with bright colors.`,
+    pt: ``,
+  },
+  "perito-art-levels": {
+    en: `In reference to an e-mail inbox, the levels are displayed like new emails, each unlocked level becoming a new email at the top of the list. Levels also have a slot for a completion badge, being empty when a level has not been completed, having a green check mark to show that a level has been completed, or a shiny shield medal for when a level has been completed without errors.`,
+    pt: ``,
+  },
   "cibernauta-title": {
-    en: `Cibernauta`,
-    pt: `Cibernauta`,
+    en: `<a href="https://cibernauta-frontend.vercel.app" target="_blank">Cibernauta</a>`,
+    pt: `<a href="https://cibernauta-frontend.vercel.app" target="_blank">Cibernauta</a>`,
   },
   "wormsweeper-title": {
-    en: `Worm Sweeper`,
-    pt: `Worm Sweeper`,
+    en: `<a href="https://bruno-rubim.github.io/worm_sweeper" target="_blank">Worm Sweeper</a>`,
+    pt: `<a href="https://bruno-rubim.github.io/worm_sweeper" target="_blank">Worm Sweeper</a>`,
   },
   "miniscale-title": {
-    en: `Mini-Scale`,
-    pt: `Mini-Scale`,
+    en: `<a href="https://poryphyr73.itch.io/mini-scale" target="_blank">Mini-Scale</a>`,
+    pt: `<a href="https://poryphyr73.itch.io/mini-scale" target="_blank">Mini-Scale</a>`,
   },
   "website-title": {
     en: `Personal Website`,
@@ -127,6 +165,14 @@ const texts = {
     en: `Site and Circuit`,
     pt: `Site and Circuit`,
   },
+  "dbirl-title": {
+    en: `Death Battle in Real Life`,
+    pt: `Batalha até a Morte na Vida Real`,
+  },
+  "discbot-title": {
+    en: `DiscBot`,
+    pt: `DiscBot`,
+  },
 };
 
 let leng = localStorage.getItem("language");
@@ -134,6 +180,9 @@ if (!leng) {
   leng = "en";
 }
 
+/**
+ * Checks for elements with its id as a keys in the texts object and replaces its innerHTML with the corresponding string and current set language
+ */
 function translate() {
   for (const key in texts) {
     const element = document.querySelector("#" + key);
